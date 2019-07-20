2eb2fb51597479dd1b9e60838e3bd48bff29e3a5
jingmiaowei <jingmiaowei@gmail.com>
2019-07-20 17:42:01 -0500

    workspace

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,11 +1,28 @@
-from django.urls import path
+"""Todolist URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.urls import path,include
+from django.contrib import admin
+
+
+from . import TodoListView,TodoDetailView,TodoCreateView,TodoUpdateView,TodoDeleteView
 
 urlpatterns = [
-    path('', TodoListView.as_view(), name='home'),
-    path('todo/<int:pk>/', TodoDetailView.as_view(), name='list_detail'),
-    path('todo/new/', TodoCreateView.as_view(), name='make_list'),
-    path('todo/edit/<int:pk>', TodoUpdateView.as_view(), name='update_list'),
-    path('todo/delete/<int:pk>', TodoDeleteView.as_view(), name='delete_list'),
+    path(r'', TodoListView.as_view(), name='home'),
+    path(r'^todo/<int:pk>/$', TodoDetailView.as_view(), name='list_detail'),
+    path(r'^todo/new/$', TodoCreateView.as_view(), name='make_list'),
+    path(r'^todo/edit/<int:pk>$', TodoUpdateView.as_view(), name='update_list'),
+    path(r'^todo/delete/<int:pk>$', TodoDeleteView.as_view(), name='delete_list'),
 ]
--- Todolist/settings.py
+++ Todolist/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Todo'
-    'imagekit'
     'rest_framework'
     'api'
 )
